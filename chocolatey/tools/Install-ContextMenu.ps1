8dd737646a61f41e16eabe4a8fbba4b527a11345
Michael Kropat <mail@michael.kropat.name>
2015-08-18 01:56:59 +0000

    Add "Edit with Vim" context menu action

---
+++ chocolatey/tools/Install-ContextMenu.ps1
@@ -0,0 +1,24 @@
+﻿function Get-GvimExePath {
+    # Find Vim directory from the *Cream* Vim Installer's UninstallString
+    $is64bit = [IntPtr]::size -eq 8
+    if ($is64bit) {
+        $hklmSoftwareWindows = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion'
+    } else {
+        $hklmSoftwareWindows = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+    }
+    $uninstallVim = Join-Path $hklmSoftwareWindows 'Uninstall\Vim'
+    $uninstallString = (Get-ItemProperty $uninstallVim UninstallString).UninstallString
+    $installDir = Split-Path -Parent $uninstallString
+    return Join-Path $installDir 'gvim.exe'
+}
+
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT `
+    -ErrorAction SilentlyContinue | Out-Null
+    
+Set-Location -LiteralPath HKCR:\*\shell
+New-Item -Name vim -Force | Out-Null
+Set-Item -Path vim -Value 'Edit with &Vim'
+
+$path = Get-GvimExePath
+New-Item -Path vim -Name command | Out-Null
+Set-Item -Path vim\command -Value """${path}"" ""%1"""
--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -2,9 +2,13 @@ $packageName = 'vim-dwiw2015'
 
 $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $bootstrapPath = Join-Path $toolsPath bootstrap.ps1
+$installCtxPath = Join-Path $toolsPath 'Install-ContextMenu.ps1'
 
 try {
     & $bootstrapPath
+
+    Start-ChocolateyProcessAsAdmin "& `'$installCtxPath`'"
+
     Write-ChocolateySuccess "$packageName"
 } catch {
     Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
